674d7714c6fa3d107ad380466f2105b45d5e8998
Iskander Sharipov <quasilyte@gmail.com>
2021-04-07 21:39:00 +0300

    random: use textContent instead of innerText

--- random/files/sloppyErrorCheck.go
+++ random/files/sloppyErrorCheck.go
@@ -3,8 +3,8 @@ package gorules
 import "github.com/quasilyte/go-ruleguard/dsl"
 
 func sloppyErrorCheck(m dsl.Matcher) {
-	/* Checking error without assigning it to a variably,
-	   like in `if f() != nil`. */
+	// Checking error without assigning it to a variably,
+	// like in `if f() != nil`.
 	m.Match(`$err != nil`,
 		`$err == nil`).
 		Where(!m["err"].Pure && m["err"].Type.Is(`error`)).
--- random/files/underlyingError.go
+++ random/files/underlyingError.go
@@ -3,7 +3,7 @@ package gorules
 import "github.com/quasilyte/go-ruleguard/dsl"
 
 func underlyingError(m dsl.Matcher) {
-	/* See https://twitter.com/dvyukov/status/1174698980208513024 */
+	// See https://twitter.com/dvyukov/status/1174698980208513024
 	m.Match(`type $x error`).
 		Report(`error as underlying type is probably a mistake`).
 		Suggest(`type $x struct { error }`)
