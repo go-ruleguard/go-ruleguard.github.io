639dbf2ab746e75a1af2e409da2b4cbdf83345b9
Iskander Sharipov <quasilyte@gmail.com>
2021-04-24 23:07:59 +0300

    random: fix emptyStringComparison.go

--- random/files/emptyStringComparison.go
+++ random/files/emptyStringComparison.go
@@ -5,9 +5,9 @@ import "github.com/quasilyte/go-ruleguard/dsl"
 func emptyStringComparison(m dsl.Matcher) {
 	m.Match(`len($s) == 0`).
 		Where(m["s"].Type.Is(`string`)).
-		Report(`replace $$ with len($s) == ""`)
+		Report(`replace $$ with $s == ""`)
 
 	m.Match(`len($s) != 0`).
 		Where(m["s"].Type.Is(`string`)).
-		Report(`replace $$ with len($s) != ""`)
+		Report(`replace $$ with $s != ""`)
 }
